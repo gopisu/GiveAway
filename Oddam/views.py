809eeb8785642b76e336c3492baca6fa2a7aec66
gopisu <malgorzata.pisula@gmail.com>
2020-02-22 19:33:34 +0100

    change landing page to index

--- Oddam/views.py
+++ Oddam/views.py
@@ -5,7 +5,7 @@ from Oddam.models import Donation, Institution
 from django.views import View
 import random
 
-class LandingPageView(View):
+class AddDonationView(View):
     def get(self, request):
         return render(request, "Oddam/form.html")
 
@@ -15,11 +15,11 @@ class ConfirmationView(View):
         return render(request, "Oddam/form-confirmation.html")
 
 
-class AddDonationView(View):
+class LandingPageView(View):
     def get(self, request):
         donations_number = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
         institutions_donated = Donation.objects.values('institution_id').distinct().count()
-        random_fund = Institution.objects.all().order_by("?")[:3]
+        random_fund = Institution.objects.all().filter(type='FUND').order_by("?")[:3]
         return render(request, "Oddam/index.html", {'donations_number': donations_number,
                                                     'institutions_donated': institutions_donated,
                                                     'random_fund': random_fund})
