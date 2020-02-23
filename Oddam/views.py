7e20b748fbbe4658948faa7f4395c125418fd3f7
gopisu <malgorzata.pisula@gmail.com>
2020-02-23 12:42:30 +0100

    change landing page to index

--- Oddam/views.py
+++ Oddam/views.py
@@ -20,9 +20,13 @@ class LandingPageView(View):
         donations_number = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
         institutions_donated = Donation.objects.values('institution_id').distinct().count()
         random_fund = Institution.objects.all().filter(type='FUND').order_by("?")[:3]
+        random_org = Institution.objects.all().filter(type='ORG').order_by("?")[:4]
+        random_gat = Institution.objects.all().filter(type='GAT').order_by("?")[:3]
         return render(request, "Oddam/index.html", {'donations_number': donations_number,
                                                     'institutions_donated': institutions_donated,
-                                                    'random_fund': random_fund})
+                                                    'random_fund': random_fund,
+                                                    'random_gat': random_gat,
+                                                    'random_org': random_org})
 
 
 class LoginView(View):
